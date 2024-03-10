d401d23c98c557828b5c9b2de1bb4727968082cb
ohhamma <judyham10634@naver.com>
2024-03-10 20:59:10 +0900

    feat: link to postgresql (#3)

--- mypong/mypong/settings.py
+++ mypong/mypong/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-^%djeee16j-%@i2%zsqmlt20=*1%(_l)4m04@w%-+=^(rnhvd@
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,17 +69,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mypong.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mypong',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -101,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -112,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- mypong/users/models.py
+++ mypong/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    STATUS_CHOICES = (
+        ('OF', 'Offline'),
+        ('ON', 'Online'),
+        ('GA', 'Gaming'),
+    )
+
+    id = models.CharField(
+        max_length=10,
+        primary_key=True,
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default='OF',
+    )
+    message = models.TextField(
+        blank=True,
+    )
+    wins = models.IntegerField(
+        default=0,
+    )
+    loses = models.IntegerField(
+        default=0,
+    )
+
+    def __str__(self):
+        return self.id
