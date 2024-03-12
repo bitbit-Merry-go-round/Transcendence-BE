59a8e0a20f521aed2404a8210c120cc77eb1c25f
ohhamma <judyham10634@naver.com>
2024-03-12 17:14:32 +0900

    feat: add Friend model (#3)

--- mypong/users/models.py
+++ mypong/users/models.py
@@ -2,30 +2,41 @@ from django.db import models
 
 
 class User(models.Model):
+    friends = models.ManyToManyField("User", blank="True")
+
     STATUS_CHOICES = (
         ('OF', 'Offline'),
         ('ON', 'Online'),
-        ('GA', 'Gaming'),
+        ('GA', 'Gaming')
     )
 
-    id = models.CharField(
+    uid = models.CharField(
         max_length=10,
-        primary_key=True,
+        primary_key=True
     )
     status = models.CharField(
         max_length=2,
         choices=STATUS_CHOICES,
-        default='OF',
+        default='OF'
     )
     message = models.TextField(
         blank=True,
+        default=''
     )
     wins = models.IntegerField(
-        default=0,
+        default=0
     )
     loses = models.IntegerField(
-        default=0,
+        default=0
     )
 
     def __str__(self):
-        return self.id
+        return self.uid
+
+
+class Friend(models.Model):
+    from_user = models.ForeignKey(User, related_name='from_user', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='to_user', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.from_user, self.to_user
