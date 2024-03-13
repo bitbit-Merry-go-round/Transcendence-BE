6932a10ec1217f6fb3deee208f0d51ffeee7a2c4
ohhamma <judyham10634@naver.com>
2024-03-13 22:05:52 +0900

    feat: patch user profile (#6)

--- mypong/user_profile/views.py
+++ mypong/user_profile/views.py
@@ -4,11 +4,6 @@ from users.models import User
 from .serializers import UserSerializer
 
 
-class UserCreationAPI(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetailAPI(generics.RetrieveAPIView):
+class UserDetailAPI(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
