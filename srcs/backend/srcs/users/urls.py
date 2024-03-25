ce80e84a5e2204c18c4469c0be186f58fe6304b2
ohhamma <judyham10634@naver.com>
2024-03-25 21:19:09 +0900

    feat: add TokenRefreshView (#13)

--- srcs/backend/srcs/users/urls.py
+++ srcs/backend/srcs/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import (
     UserProfileAPIView,
@@ -13,6 +14,7 @@ app_name = 'users'
 urlpatterns = [
     path('register/', UserRegisterAPIView.as_view()),
     path('login/', UserLoginAPIView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
 
     path('<pk>/profile/', UserProfileAPIView.as_view()),
     path('<str:from_user>/friends/', FriendListAPI.as_view()),
