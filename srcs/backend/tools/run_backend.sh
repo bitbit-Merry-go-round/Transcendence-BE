22ede00e24a9b990f986d0eb597b50a3ac8b906c
jeongyunnim <jeongyun7521@gmail.com>
2024-04-07 19:20:55 +0900

    docs: Add env directions. Related with 2FA. (#43)

--- srcs/backend/tools/run_backend.sh
+++ srcs/backend/tools/run_backend.sh
@@ -11,6 +11,14 @@ BACKEND_DB_HOST=${BACKEND_DB_HOST}
 BACKEND_DB_USER=${BACKEND_DB_USER}
 BACKEND_DB_PW=${BACKEND_DB_PW}
 BACKEND_DB_PORT=${BACKEND_DB_PORT}
+
+USER_MANAGER_PORT=${USERS_PORT}
+
+EMAIL_HOST=${EMAIL_HOST}
+EMAIL_PORT=${EMAIL_PORT}
+EMAIL_USE_TLS=${EMAIL_USE_TLS}
+EMAIL_HOST_USER=${EMAIL_HOST_USER}
+EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}
 EOF
 
 python manage.py makemigrations
@@ -18,8 +26,6 @@ python manage.py makemigrations
 python manage.py migrate
 
 python manage.py createsuperuser --no-input
-
-python ./add_user.py
 
 # todo ssl 가장 나중에 하자. 그리고 오류 생김. sslserver 구동할 수 있게 패키지 받아야 함. django settings에 INSTALLED_APP에도 설정 해야 함.
 # python manage.py runsslserver --certificate /etc/ssl/private/domain.crt --key /etc/ssl/private/domain.key 0:8000
