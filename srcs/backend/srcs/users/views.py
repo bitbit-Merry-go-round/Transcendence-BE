8e44de809ca478f28e92bb875bab255b086514bf
ohhamma <judyham10634@naver.com>
2024-04-12 19:16:00 +0900

    fix: path from user-manager (#61)

--- srcs/backend/srcs/users/views.py
+++ srcs/backend/srcs/users/views.py
@@ -26,35 +26,21 @@ class RouteToUserManagerAPIView(APIView):
                 "detail": "invalid access token"
             }, status=status.HTTP_401_UNAUTHORIZED)
 
-        bearer, _, token = token.partition(' ')
-        payload = jwt.decode(jwt=token, key=env("SECRET_KEY"), algorithms=['HS256'])
-        username = payload.get("user_id")
-
-        query = request.META.get("QUERY_STRING")
-
         user_manager_scheme = request.scheme
         user_manager_port = env("USER_MANAGER_PORT")
         user_manager_path = request.path
 
-        if user_manager_path == "/users/me/profile/":
-            user_manager_path = "/users/" + username + "/profile/"
-        if user_manager_path == "/users/me/friends/":
-            user_manager_path = "/users/" + username + "/friends/"
-
         user_manager_url = f"{user_manager_scheme}://{USER_MANAGER_HOST_NAME}:{user_manager_port}{user_manager_path}"
 
+        query = request.META.get("QUERY_STRING")
         if query != "":
             user_manager_url = user_manager_url + f"?{query}"
 
+        bearer, _, token = token.partition(' ')
         response = requests.get(
             user_manager_url,
             headers={"Authorization": f"Bearer {token}"}
         )
-
-        if response.status_code == 404:
-            return JsonResponse({
-                "detail": "invalid request path"
-            }, status=response.status_code)
 
         return HttpResponse(
             content=response.content,
@@ -69,20 +55,14 @@ class RouteToUserManagerAPIView(APIView):
                 "detail": "invalid access token"
             }, status=status.HTTP_401_UNAUTHORIZED)
 
-        bearer, _, token = token.partition(' ')
-        payload = jwt.decode(jwt=token, key=env("SECRET_KEY"), algorithms=['HS256'])
-        username = payload.get("user_id")
-
         user_manager_scheme = request.scheme
         user_manager_port = env("USER_MANAGER_PORT")
         user_manager_path = request.path
 
-        if "me" in user_manager_path:
-            user_manager_path = user_manager_path.replace("me", username)
-
         user_manager_url = f"{user_manager_scheme}://{USER_MANAGER_HOST_NAME}:{user_manager_port}{user_manager_path}"
         content_type = request.headers.get("Content-Type")
 
+        bearer, _, token = token.partition(' ')
         response = requests.patch(
             user_manager_url,
             data=request.body,
@@ -105,20 +85,14 @@ class RouteToUserManagerAPIView(APIView):
                 "detail": "invalid access token"
             }, status=status.HTTP_401_UNAUTHORIZED)
 
-        bearer, _, token = token.partition(' ')
-        payload = jwt.decode(jwt=token, key=env("SECRET_KEY"), algorithms=['HS256'])
-        username = payload.get("user_id")
-
         user_manager_scheme = request.scheme
         user_manager_port = env("USER_MANAGER_PORT")
         user_manager_path = request.path
 
-        if "me" in user_manager_path:
-            user_manager_path = user_manager_path.replace("me", username)
-
         user_manager_url = f"{user_manager_scheme}://{USER_MANAGER_HOST_NAME}:{user_manager_port}{user_manager_path}"
         content_type = request.headers.get("Content-Type")
 
+        bearer, _, token = token.partition(' ')
         response = requests.post(
             user_manager_url,
             data=request.body,
@@ -141,19 +115,13 @@ class RouteToUserManagerAPIView(APIView):
                 "detail": "invalid access token"
             }, status=status.HTTP_401_UNAUTHORIZED)
 
-        bearer, _, token = token.partition(' ')
-        payload = jwt.decode(jwt=token, key=env("SECRET_KEY"), algorithms=['HS256'])
-        username = payload.get("user_id")
-
         user_manager_scheme = request.scheme
         user_manager_port = env("USER_MANAGER_PORT")
         user_manager_path = request.path
 
-        if "me" in user_manager_path:
-            user_manager_path = user_manager_path.replace("me", username)
-
         user_manager_url = f"{user_manager_scheme}://{USER_MANAGER_HOST_NAME}:{user_manager_port}{user_manager_path}"
 
+        bearer, _, token = token.partition(' ')
         response = requests.delete(
             user_manager_url,
             headers={"Authorization": f"Bearer {token}"}
@@ -164,5 +132,3 @@ class RouteToUserManagerAPIView(APIView):
             status=response.status_code,
             headers=response.headers
         )
-
-    http_method_names = ['get', 'post', 'patch', 'delete', 'options']
--- srcs/user-manager/srcs/users/urls.py
+++ srcs/user-manager/srcs/users/urls.py
@@ -1,19 +1,21 @@
 from django.urls import path
 
 from .views import (
+    MyProfileAPIView,
     MyFriendAPIView,
-    FriendDeleteAPIView,
-    UserSearchAPIView,
+    MyFriendDeleteAPIView,
     UserProfileAPIView,
     UserCreationAPIView,
+    UserSearchAPIView,
 )
 
 app_name = 'users'
 
 urlpatterns = [
+    path('me/profile/', MyProfileAPIView.as_view()),
+    path('me/friends/', MyFriendAPIView.as_view()),
+    path('me/friends/<str:to_user>/', MyFriendDeleteAPIView.as_view()),
     path('<str:username>/profile/', UserProfileAPIView.as_view()),
-    path('<str:username>/friends/', MyFriendAPIView.as_view()),
-    path('<str:from_user>/friends/<str:to_user>/', FriendDeleteAPIView.as_view()),
     path('create/', UserCreationAPIView.as_view()),
 
     path('', UserSearchAPIView.as_view()),
